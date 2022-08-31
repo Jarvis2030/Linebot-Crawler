ac4afc5efab427727c79d2daf929059bb0aa85aa
Jarvis2020 <ylindc@connect.ust.hk>
2022-08-31 21:23:40 +0800

    google API setup

--- Linebot.py
+++ Linebot.py
@@ -5,6 +5,7 @@ import time # get the current time
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import (MessageEvent, JoinEvent, TextMessage, TextSendMessage)
+import quickstart
 
 Line_bot_api = LineBotApi('mGZ+2slcF0VcNjs1YvreNpOm3h4STfJqPUzTonl64akZ79ieFSZjgvHU/PFx44dNBbzfcL5odxlvtcMcDaPeriu2A7LD43fPhkrOHzcS4Y5MSBg6gmto7Jqv15fOuODPKc2NdPWWiR108i0ti4u1VwdB04t89/1O/w1cDnyilFU=') # Linebot customize Api
 handler = WebhookHandler('008021c29864de0a406566a9ad5e01de') # channel secret
@@ -20,22 +21,12 @@ def callback(): #checking if the input message is right
         abort(400)
     return 'OK'
 
-@handler.add(MessageEvent, message = TextMessage)
+#鸚鵡程式: 測試用
+'''@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    Line_bot_api.reply_message(event.reply_token, TextSendMessage(text = event.message.text))
-
-'''@handler.add(MessageEvent, message = TextMessage) #The text Message you got from Linbot
-def handle_msg(event):
-    #Line_bot_api.push_message('Testing: Peronsal ID or Group ID', TextSendMessage(text=''))
-    if(event.message.text=="start"):
-        message=TextSendMessage(event.source.group_id)
-        Line_bot_api.reply_message(event.reply_token, message)''' #get the group ID
-    
-    
-
-
-
-
+    Line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))'''
 
 
 
